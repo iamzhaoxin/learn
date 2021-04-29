0fef58d8326e15fc7478b3ca65b351b5489da525
赵鑫 <zhaoxin130120@126.com>
2021-04-29 15:38:31 +0800

    update 完成 藏品

--- MuseumProject/Museum/Museum/spiders/1114.py
+++ MuseumProject/Museum/Museum/spiders/1114.py
@@ -31,13 +31,11 @@ class M1114(scrapy.Spider):
             # yield scrapy.Request(url=exh_url, callback=self.exh_parse, meta={'exh_id': exh_id})
 
     # 藏品列表
-
-    # TODO 解析并进入藏品详情页
     def cols_parse(self, response):
-        col_urls = response.xpath('/html/body/div[3]/div[2]/div[2]/div[1]/dl//@href').extract()
+        col_urls = response.xpath('/html/body/div/div[3]/div[2]/ul//@href').extract()
         for col_url in col_urls:
             response.meta['col_id'] += 1
-            col_url = "http://www.luxunmuseum.com.cn/" + col_url
+            col_url = col_url.replace('../../','http://www.pgm.org.cn/')
             yield scrapy.Request(url=col_url, callback=self.col_parse, meta={'col_id': response.meta['col_id']})
         return
 
@@ -45,20 +43,22 @@ class M1114(scrapy.Spider):
     def col_parse(self, response):
         item = Item()
         item['exh_id'] = item['exh_name'] = item['exh_info'] = item['exh_picture'] = item['exh_time'] = ''
-        item['mus_name'] = '北京鲁迅博物馆'
-        item['mus_id'] = 1106
+        item['mus_name'] = '文化部恭王府博物馆'
+        item['mus_id'] = 1114
         item['col_id'] = response.meta['col_id']
-        item['col_name'] = response.xpath('/html/body/div[3]/div[2]/div[2]/div[1]/text()')[0].extract()
-        item['col_info'] = response.xpath('/html/body/div[3]/div[2]/div[2]/div[3]')[0].xpath('string(.)')[
+        # TODO
+        item['col_name'] = response.xpath('/html/body/div/div[2]/div/div[2]/div[@class="title"]/text()')[0].extract()
+        item['col_info'] = response.xpath('/html/body/div/div[2]/div/div[2]/div[@class="pages_content"]')[0].xpath('string(.)')[
             0].extract().strip()
-        item['col_era'] = '近代'
-        item['col_picture'] = "http://www.luxunmuseum.com.cn/" + \
-                              response.xpath('/html/body/div[3]/div[2]/div[2]//@src')[0].extract()
+        item['col_era'] = '不详'
+        item['col_picture'] = "http://www.pgm.org.cn/" + \
+                              response.xpath('/html/body/div/div[2]/div/div[2]/div[@class="pages_content"]//@src')[0].extract()
         yield item
         print("正在爬取藏品 " + item['col_name'] + " ing")
         return
 
     # 展览列表
+    #TODO
     def exh_parse(self, response):
         item = Item()
         item['col_id'] = item['col_name'] = item['col_info'] = item['col_era'] = item['col_picture'] = ''
