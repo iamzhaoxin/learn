98af1619cbe1f9d5b86e631a90bd05061a12e255
赵鑫 <zhaoxin-software@outlook.com>
2021-12-30 10:32:21 +0800

    Java学习代码
    基础语法
    - 注释

---
+++ learn-Java/src/basic/grammar/Annotation.java
@@ -0,0 +1,18 @@
+package basic.grammar;
+
+/**
+ * 可以被JavaDoc工具软件识别的标签：
+ * @author zhaoxin
+ * @docRoot path
+ * @version 1.0
+ */
+public class Annotation {
+    public static void main(String[] args) {
+        //单行注释
+
+       /*
+       多行注释
+       多行注释
+        */
+    }
+}
