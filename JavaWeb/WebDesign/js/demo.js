62316a084e41ff93d0e02814d75759141f9ac6ce
赵鑫 <zhaoxin-software@outlook.com>
2022-01-24 23:13:15 +0800

    新建WebDesign模块 开始学习JavaScript

---
+++ JavaWeb/WebDesign/js/demo.js
@@ -0,0 +1,15 @@
+/**
+ * notice 语法&输出
+ * - 区分大小写
+ * - 每行结尾的分号可有可无
+ * - 大括号表示代码块
+ * - 输出语句
+ *     - windows.alert()   弹出警告框
+ *     - document.write()  写入HTML输出
+ *     - console.log()     写入浏览器控制台
+ *
+ */
+
+alert("alert:hello js");
+document.write("write:hello js");
+console.log("log:hello js");
