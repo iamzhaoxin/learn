f32a0ea920592dcfdfe7ee551336cbc2809e0ecd
赵鑫 <zhaoxin-software@outlook.com>
2022-02-02 23:33:01 +0800

    初识Spring

---
+++ SSM/Spring/spring_ioc/src/main/java/dao/impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package dao.impl;
+
+import dao.UserDao;
+
+/**
+ * @Author: 赵鑫
+ * @Date: 2022/2/2 23:09
+ */
+public class UserDaoImpl implements UserDao {
+    public void save() {
+        System.out.println("save running...");
+    }
+}
