8d883d63ba6566ad3486da4778bc11cfa00fee10
赵鑫 <zhaoxin-software@outlook.com>
2022-07-02 21:10:45 +0800

    CharSource & CharSink

---
+++ guava/src/test/java/io/CharSinkTest.java
@@ -0,0 +1,20 @@
+package io;
+
+import org.junit.Test;
+
+/**
+ * @Author: 赵鑫
+ * @Date: 2022/7/2
+ */
+public class CharSinkTest {
+
+    /**
+     * CharSource --> Reader
+     *
+     * CharSink --> Writer
+     */
+    @Test
+    public void testCharSink(){
+
+    }
+}
