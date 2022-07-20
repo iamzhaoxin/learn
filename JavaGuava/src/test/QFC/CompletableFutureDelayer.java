170ab35d79add9249f335fcccb7797985816d9af
赵鑫 <zhaoxin-software@outlook.com>
2022-07-20 23:55:03 +0800

    用_守护线程_结束CompletableFuture的任务线程！！

--- JavaGuava/src/test/QFC/CompletableFutureDelayer.java
+++ JavaGuava/src/test/QFC/CompletableFutureDelayer.java
@@ -1,4 +1,5 @@
 import com.sun.istack.internal.NotNull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.*;
 import java.util.function.BiConsumer;
@@ -8,17 +9,25 @@ import java.util.function.Function;
  * @Author: 赵鑫
  * @Date: 2022/7/20
  */
+@Slf4j
 public class CompletableFutureDelayer {
+
+    static final ScheduledThreadPoolExecutor delayer;   // delayer：线程数=1的定时任务线程池
 
     static final class DaemonThreadFactory implements ThreadFactory {
         public Thread newThread(Runnable r) {
             Thread t = new Thread(r);
+            /*
+                使用线程池，默认线程工厂创建的都是用户线程，核心线程会一直存在
+                    - 在程序结束时，要用shutdown关闭线程池，否则线程池资源一直不释放，JVM不退出
+                    - 或者用setDaemon(true)设为守护进程。主线程结束时，守护进程就会结束
+             */
             t.setDaemon(true);
             t.setName("CompletableFutureDelayScheduler");
             return t;
         }
     }
-    static final ScheduledThreadPoolExecutor delayer;
+
     static {
         delayer = new ScheduledThreadPoolExecutor(1, new DaemonThreadFactory());
         delayer.setRemoveOnCancelPolicy(true);
@@ -26,22 +35,20 @@ public class CompletableFutureDelayer {
 
     public static <T> CompletableFuture<T> timeout(CompletableFuture<T> future, long delay, TimeUnit unit) {
         final CompletableFuture<T> timer = new CompletableFuture<>();
-        Runnable runnable=()->timer.completeExceptionally(new TimeoutException());
-        // 计时器开始计时，定时任务：runnable。runnable执行结束后抛出TimeoutException异常
-        ScheduledFuture<?> scheduledFuture = delayer.schedule(runnable, delay, unit);
+        Runnable runnable = () -> timer.completeExceptionally(new TimeoutException());
+        // 开始定时执行runnable, runnable执行结束后抛出TimeoutException异常
+        delayer.schedule(runnable, delay, unit);
 
-        future.applyToEither(timer, Function.identity()).exceptionally(throwable -> {
-            System.out.println(throwable);
-            CompletableFuture<T> futureReturn = new CompletableFuture<>();
-            futureReturn.completeExceptionally(throwable);
-            try {
-                return futureReturn.get();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+        return future.applyToEither(timer, Function.identity()).exceptionally(throwable -> {
+            Throwable t = throwable;
+            while (t.getCause() != null) {
+                t = t.getCause();
             }
+            if(t instanceof TimeoutException){
+                log.info("这里执行超时处理");
+            }
+            return null;
         });
-
-        return future;
     }
 
 }
--- JavaGuava/src/test/QFC/CompletableFutureDelayerTest.java
+++ JavaGuava/src/test/QFC/CompletableFutureDelayerTest.java
@@ -1,3 +1,5 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -6,11 +8,13 @@ import java.util.concurrent.TimeUnit;
  * @Author: 赵鑫
  * @Date: 2022/7/20
  */
+@Slf4j
 public class CompletableFutureDelayerTest {
     public static void main(String[] args) {
+        // CompletableFuture异步任务 默认采用的线程是ForkJoinPool创建的守护线程
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(3000L);
+                Thread.sleep(5000L);
                 System.out.println("我还活着");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -19,9 +23,12 @@ public class CompletableFutureDelayerTest {
         });
         CompletableFuture<String> futureDelay = CompletableFutureDelayer.timeout(future, 1500, TimeUnit.MILLISECONDS);
         try {
-            System.out.println(futureDelay.get());
+            if(futureDelay.get()!=null){
+                log.info(futureDelay.get());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        // 当主线程结束时，所有守护线程都结束（所以线程池ScheduledThreadPoolExecutor要自定义线程工厂，将线程池里的子线程设为非默认的守护线程
     }
 }
